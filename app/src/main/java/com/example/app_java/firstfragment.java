cfa8db381a8ac6496732d2e737a8dc969ad2b1c0
Seba <pezkaos@gmail.com>
2020-08-26 19:47:02 -0500

    ADD: Error en .java

---
+++ app/src/main/java/com/example/app_java/firstfragment.java
@@ -0,0 +1,42 @@
+package com.example.app_java.;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+class FirstFragment extends Fragment {
+
+    public void validaPass (String pass){
+        if (pass.equals("123Pass")){
+
+
+        }
+
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_firstfragment, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.boton1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        }
+    }
+}
